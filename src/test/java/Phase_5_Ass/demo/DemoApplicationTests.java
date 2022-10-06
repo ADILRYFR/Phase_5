259de62bdb0d0c199e10f852cdd8fbd55716dbd3
frogface-2018 <105274021+frogface-2018@users.noreply.github.com>
2022-10-06 06:30:11 +0530

    Add files via upload

---
+++ src/test/java/Phase_5_Ass/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Phase_5_Ass.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
